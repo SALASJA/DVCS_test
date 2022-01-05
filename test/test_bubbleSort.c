125ffd6151d33d077cb60d88d502ce2eb6a0f744
Jorge Salas <jasalas@alumni.uncg.edu>
2022-01-05 04:42:52 -0500

    finished bubbleSort task

--- test/test_bubbleSort.c
+++ test/test_bubbleSort.c
@@ -2,7 +2,20 @@
 #include "../tools/tools.h"
 
 int main(){
-
+	int A[] = {5,4,3,2,1};
+	printf("before: ");
+	for(int i = 0; i < 5; i++){
+		printf("%d ", A[i]);
+	}
+	printf("\n");
+	
+	bubbleSort(A,5);
+	
+	printf("after: ");
+	for(int i = 0; i < 5; i++){
+		printf("%d ", A[i]);
+	}
+	printf("\n");
 
 	return 0;
 }
--- tools/tools.c
+++ tools/tools.c
@@ -6,5 +6,16 @@ void printArray(int * A, int size){
 
 
 void bubbleSort(int * A, int size){
-
+	bool sorted = false;
+	while(!sorted){
+		sorted = true;
+		for(int i = 0; i < size; i++){
+			if(A[i] > A[i + 1]){
+				int temp = A[i];
+				A[i] = A[i + 1];
+				A[i + 1] = temp;
+				sorted = false;
+			}
+		}
+	}
 }
